e5d5f8b687060410e009250b26593c28ee5a21ae
Jaemin Jung <windyvill@gmail.com>
2019-11-20 16:31:27 +0900

    산 구현
    산 이미지 구현

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+
+    [SerializeField] float spped = 1.0f;
+    [SerializeField] float startPosition;
+    [SerializeField] float endPosition;
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(-1 * spped * Time.deltaTime, 0, 0);
+        if (transform.position.x <= endPosition)
+        {
+            transform.Translate(-1 * (endPosition - startPosition), 0, 0);
+
+            SendMessage("ChangePosition", SendMessageOptions.DontRequireReceiver);
+        }
+
+    }
+
+
+
+
+}
